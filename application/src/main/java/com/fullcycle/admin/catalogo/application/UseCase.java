0b8cd7b7badb8752c93e27827a5f875fc9e2a01a
fukudaum <fukudaum@gmail.com>
2024-07-19 22:35:44 -0300

    Add category tests

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.fullcycle.admin.catalogo.application;
 
-import com.fullcycle.admin.catalogo.domain.Category;
+import com.fullcycle.admin.catalogo.domain.category.Category;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
